f8410be466b6ddd8e65b109d3dc669e0060307d8
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2022-08-21 11:51:41 +1000

    2nd commit, data now saves in data.json

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
 const fs = require('fs');
-
-// fs.readFile('./data.json', 'utf-8', (err, jsonString) =>{
-//     if(err) {
-//         console.log(err);
-//     } else {
-//         try {
-//             const data = JSON.parse(jsonString);
-//             console.log(data.user);
-//         } catch {
-//             console.log('Error parsing JSON', err); 
-//         }
-//     } 
-// });
-
-const userMessage ={
-    name: "",
-    message: ""
-}
-
-
-const newObject = {
-    user: 'JakeyNoLs2',
-    message: 'testing sending data into JSON file'
-}; 
-
+//aim is to modulise this and call in server.js however is problematic
 function jsonReader(filePath, cb){
     fs.readFile(filePath, 'utf-8', (err, fileData)=>{
         if(err){
@@ -37,19 +13,4 @@ function jsonReader(filePath, cb){
         }
     });
 }
-
-
-
-jsonReader('./data.json', (err, data)=>{
-    if(err){
-        console.log(err);
-    } else {
-        data.userNo +=1; 
-        fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
-            if(err){
-                console.log(err);
-            }
-        });
-    }
-}); 
 
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
+
+
 var express = require('express');
-const { jsonReader } = require('./app');
+const fs = require('fs');
+
 var app = express();
 var http = require('http').Server(app);
-
+var userMessages = []; 
+var msgCount =0; 
 
 app.use(express.urlencoded({extended : true}));
 
@@ -26,12 +30,45 @@ res.sendFile(__dirname + '/www/chatroom.html');
 
 });
 
+function jsonReader(filePath, cb){
+    fs.readFile(filePath, 'utf-8', (err, fileData)=>{
+        if(err){
+            return cb && cb(err);
+        }
+        try {
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch(err){
+            return cb && cb(err); 
+        }
+    });
+}
+
+
 
 
 app.post('/chatroom', function(req, res){
+    msgCount += 1; 
+	var userResponse = req.body;
+    var userMessage = { msgNum: msgCount, 
+                        name: userResponse.name,
+                        message: userResponse.message};
+    JSON.stringify(userMessage, null, 2)
+    userMessages.push(userMessage); 
+    console.log(userMessages); 
+                
 
-	global.userMessage = req.body;
-    console.log(userMessage);
+    jsonReader('./data.json', (err, data)=>{
+    if(err){
+        console.log(err);
+    } else {
+        fs.writeFile('./data.json', JSON.stringify(userMessages, null, 2), err => {
+            if(err){
+                console.log(err);
+            }
+        });
+        }
+    });
     res.sendFile(__dirname + '/www/chatroom.html');
 
 });
